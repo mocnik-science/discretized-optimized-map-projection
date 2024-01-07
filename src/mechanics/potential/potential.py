648182877564bf713831ee04275b0abaf012572b
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2024-01-07 18:43:45 +0100

    big code cleanup

--- src/mechanics/potential/potential.py
+++ src/mechanics/potential/potential.py
@@ -58,57 +58,49 @@ class Potential:
       force = -k * sign(r)
       return energy, force
 
-
-
-
-
-
-    # D – spring constant
-    #     chosen such that the force at r = R is -r (where r is the multiple of the typical distance)
-    #     R = D * exponent * R**(exponent - 1)
-    #     => D = R**(2 - exponent) / exponent
-    if self.__D is None:
-      R = .5
-      self.__D = R**(2 - self.__exponent) / self.__exponent * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
-    if energy:
-      return self.__D * abs(r)**self.__exponent
-    elif force:
-      return -self.__D * self.__exponent * abs(r)**(self.__exponent - 1) * sign(r)
-    else:
-      energy = self.__D * abs(r)**self.__exponent
-      force = -energy * self.__exponent / r
-      return energy, force
-
-
-
-
-    # gravitational potential of a hogomeneous solid sphere
-    # m, r – mass and radius of the ‘outer’ point mass
-    # M, R – mass and radius of the big solid sphere mass
-    # gamma – gravitation constant
-    #         chosen such that the force at r = R is r (where r is the multiple of the typical distance)
-    #         R = gamma * m * M / R**2
-    #         => gamma = R**3 / (m * M)
-    m = 1
-    M = 1
-    R = .5
-    gamma = R**3 / (m * M) * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
-    sgn = sign(r)
-    r = abs(r)
-    if energy:
-      if r < R:
-        return gamma * m * M / (2 * R) * (r**2 / R**2 - 3)
-      else:
-        return -gamma * m * M / r
-    if force:
-      if r < R:
-        return gamma * m * M / R * (r**3 / R**2 + 3 / 2) * sgn
-      else:
-        return -gamma * m * M / r**2 * sgn
+    # # D – spring constant
+    # #     chosen such that the force at r = R is -r (where r is the multiple of the typical distance)
+    # #     R = D * exponent * R**(exponent - 1)
+    # #     => D = R**(2 - exponent) / exponent
+    # if self.__D is None:
+    #   R = .5
+    #   self.__D = R**(2 - self.__exponent) / self.__exponent * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
     # if energy:
-    #   return -1 / r**exponent * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
+    #   return self.__D * abs(r)**self.__exponent
+    # elif force:
+    #   return -self.__D * self.__exponent * abs(r)**(self.__exponent - 1) * sign(r)
+    # else:
+    #   energy = self.__D * abs(r)**self.__exponent
+    #   force = -energy * self.__exponent / r
+    #   return energy, force
+
+    # # gravitational potential of a hogomeneous solid sphere
+    # # m, r – mass and radius of the ‘outer’ point mass
+    # # M, R – mass and radius of the big solid sphere mass
+    # # gamma – gravitation constant
+    # #         chosen such that the force at r = R is r (where r is the multiple of the typical distance)
+    # #         R = gamma * m * M / R**2
+    # #         => gamma = R**3 / (m * M)
+    # m = 1
+    # M = 1
+    # R = .5
+    # gamma = R**3 / (m * M) * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
+    # sgn = sign(r)
+    # r = abs(r)
+    # if energy:
+    #   if r < R:
+    #     return gamma * m * M / (2 * R) * (r**2 / R**2 - 3)
+    #   else:
+    #     return -gamma * m * M / r
     # if force:
-    #   return -exponent / r**(exponent + 1) * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
+    #   if r < R:
+    #     return gamma * m * M / R * (r**3 / R**2 + 3 / 2) * sgn
+    #   else:
+    #     return -gamma * m * M / r**2 * sgn
+    # # if energy:
+    # #   return -1 / r**exponent * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
+    # # if force:
+    # #   return -exponent / r**(exponent + 1) * (1 - self._settings._dampingFactor) * self._settings._typicalDistance
 
   def _geoBearingsForCell(self, cell, neighbouringCells):
     key = cell._id2
