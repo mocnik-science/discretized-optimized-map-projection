68ce022aed26647108c3d065ad7427d308442665
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2024-02-03 04:41:17 +0100

    minor code cleanup

--- src/interfaces/gui/windows/windowMain.py
+++ src/interfaces/gui/windows/windowMain.py
@@ -2,7 +2,6 @@ import json
 import wx
 
 from src.interfaces.common.common import APP_NAME, APP_FILES_PATH
-from src.interfaces.common.interfaceCommon import InterfaceCommon
 from src.interfaces.common.projections import PROJECTION
 from src.interfaces.gui.threads.renderThread import RenderThread, EVT_RENDER_THREAD_UPDATE
 from src.interfaces.gui.threads.workerThread import WorkerThread, EVT_WORKER_THREAD_UPDATE
@@ -12,7 +11,6 @@ from src.interfaces.gui.windows.windowProj import WindowProj
 from src.interfaces.gui.windows.windowSimulationSettings import WindowSimulationSettings
 from src.geoGrid.geoGridProjectionTIN import GeoGridProjectionTIN
 from src.geoGrid.geoGridSettings import GeoGridSettings
-from src.geometry.strategy import strategyForScale
 
 class WindowMain(wx.Frame):
   def __init__(self, appSettings, viewSettings):
