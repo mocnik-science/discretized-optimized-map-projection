8d33741a223731d01dcbc4e2917abd27c5c2f510
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2024-02-24 21:04:46 +0100

    app copyright updated

--- src/interfaces/common/common.py
+++ src/interfaces/common/common.py
@@ -2,7 +2,7 @@ import os
 
 APP_NAME = 'Discretized Optimized Map Projection'
 APP_NAME_WITH_LINEBREAK = 'Discretized Optimized\nMap Projection'
-APP_COPYRIGHT = '(c) by Franz-Benjamin Mocnik, 2023'
+APP_COPYRIGHT = '(c) by Franz-Benjamin Mocnik, 2023–2024'
 APP_URL = 'https://github.com/mocnik-science/discretized-optimized-map-projection'
 APP_FILE_FORMAT = 'Discretized Optimized Map Projection file'
 APP_FILES_PATH_NON_EXPANDED = '~/.domp/'
