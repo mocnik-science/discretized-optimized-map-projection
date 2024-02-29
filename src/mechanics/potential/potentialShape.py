2c5381d20e8d6002a3ddfc19586088b0e37e385b
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2024-02-29 05:20:10 +0100

    PotentialShape improved

--- src/mechanics/potential/potentialShape.py
+++ src/mechanics/potential/potentialShape.py
@@ -8,6 +8,7 @@ class PotentialShape(Potential):
   kind = 'SHAPE'
   defaultWeight = GeoGridWeight(active=True, weightLand=.7, weightOceanActive=True, weightOcean=.3, distanceTransitionStart=100000, distanceTransitionEnd=800000)
   calibrationPossible = False
+  averaged = True
 
   def __init__(self, *args, enforceNorth=False, **kwargs):
     super().__init__(*args, **kwargs)
@@ -21,18 +22,22 @@ class PotentialShape(Potential):
     if not cell._isActive:
       return []
     forces = []
-    for i, q in enumerate(self._quantities(cell, neighbouringCells, onlyForce=True)):
+    quantities = self._quantities(cell, neighbouringCells, onlyForce=True) / len(quantities)
+    qAveraged = sum(quantities) if self.averaged else None
+    for i, q in enumerate(quantities):
       neighbouringCell = neighbouringCells[i]
-      forces.append(Force.toDestination(self.kind, neighbouringCell, Point(neighbouringCell.x + (neighbouringCell.y - cell.y), neighbouringCell.y - (neighbouringCell.x - cell.x)), q))
+      forces.append(Force.toDestination(self.kind, neighbouringCell, Point(neighbouringCell.x + (neighbouringCell.y - cell.y), neighbouringCell.y - (neighbouringCell.x - cell.x)), qAveraged if self.averaged else q))
     return forces
   def energyAndForces(self, cell, neighbouringCells):
     energy, forces = 0, []
     if not cell._isActive:
       return energy, forces
-    for i, (qEnergy, qForce) in enumerate(self._quantities(cell, neighbouringCells)):
+    quantities = self._quantities(cell, neighbouringCells)
+    qForceAveraged = sum([qForce for (_, qForce) in quantities]) / len(quantities) if self.averaged else None
+    for i, (qEnergy, qForce) in enumerate(quantities):
       energy += qEnergy
       neighbouringCell = neighbouringCells[i]
-      forces.append(Force.toCell(self.kind, neighbouringCell, Point(neighbouringCell.x + (neighbouringCell.y - cell.y), neighbouringCell.y - (neighbouringCell.x - cell.x)), qForce))
+      forces.append(Force.toCell(self.kind, neighbouringCell, Point(neighbouringCell.x + (neighbouringCell.y - cell.y), neighbouringCell.y - (neighbouringCell.x - cell.x)), qForceAveraged if self.averaged else qForce))
     return energy, forces
 
   def _values(self, cell, neighbouringCells):
@@ -43,4 +48,4 @@ class PotentialShape(Potential):
     # compute average difference
     avgDiff = 0 if self._enforceNorth else sum([Common.normalizeAngle(bearings[i] - bearingIdeal[i], intervalStart=-Common._pi) for i in range(0, lenNeighbours)]) / lenNeighbours
     # quantities
-    return [12 / (2 * Common._pi) * abs(Common.normalizeAngle(bearing - (bearingIdeal[i] + avgDiff), intervalStart=-Common._pi)) for i, bearing in enumerate(bearings)]
+    return [6 / Common._pi * abs(Common.normalizeAngle(bearing - (bearingIdeal[i] + avgDiff), intervalStart=-Common._pi)) for i, bearing in enumerate(bearings)]
